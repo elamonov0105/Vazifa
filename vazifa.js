dc81085d5c53e5a9902581e55f8835c59f39544e
EZ7-easy <b.sh.tutor@gmail.com>
2024-06-26 12:07:39 +0500

    vazifa

--- vazifa.js
+++ vazifa.js
@@ -1,46 +1,74 @@
 // 6a
-//   const soat = 10
+  const soat = 10
+
+ if (soat < 14) {
+    console.log('xayrli ton')
+  } else if (soat < 19) {
+    console.log('xayrili kun')
+    } else {
+    console.log('xayrili tun')
+  }
+
+// 6c
+ const soat = 10
+ const name = 'BUNYODbek'
+
+if (soat < 13) {
+     console.log('Xayrli tong' ${name} )
+ } else if (soat < 19) {
+     console.log('Xayrli kun'  ${name})
+ } else {
+     console.log('Xayrli tun' ${name})
+ }
+const a = soat < 13 ? 'Xayrli tong' ${name} : soat < 19 ? 'Xayrli kun' ${name} : 'Xayrli tun' ${name}
+console.log(a)
+
+
+
 //
-//  if (soat < 14) {
-//     console.log('xayrli ton')
-//   } else if (soat < 19) {
-//     console.log('xayrili kun')
-//     } else {
-//     console.log('xayrili tun')
-//   }
-
-//6c
-//  const soat = 10
-//  const name = 'BUNYODbek'
-//
-// if (soat < 13) {
-//      console.log(Xayrli tong ${name} )
-//  } else if (soat < 19) {
-//      console.log(Xayrli kun ${name})
-//  } else {
-//      console.log(Xayrli tun ${name})
-//  }
-// const a = soat < 13 ? Xayrli tong ${name} : soat < 19 ? Xayrli kun ${name} : Xayrli tun ${name}
-// console.log(a)
-
-
-
-
 // 6D
-//     const yosh =
-//
-//  const a = yosh <= 6
-//   const b = yosh >= 65
-//
-//  if (a || b) {
-//     console.log('chegirma')
-//  }else{
-//     console.log('chegirma yoq')
-//   }
-//
-//     console.log(yosh <= 6 || yosh >= 65)
-//  const c = a || b ? 'chegirma' : 'chegirma yoq'
-// console.log(c)
+    const yosh = 35
+
+ const a = yosh <= 6
+  const b = yosh >= 65
+
+ if (a || b) {
+    console.log('chegirma')
+ }else{
+    console.log('chegirma yoq')
+  }
+
+    console.log(yosh <= 6 || yosh >= 65)
+ const c = a || b ? 'chegirma' : 'chegirma yoq'
+console.log(c)
+
+const a = Math.random()
+console.log(a)
 
 
+// 6f
+if (a < 0.5){
+    console.log('chikka')
+} else {
+    console.log('pukka')
+}
+const d = a < 0.5 ? 'chikka' : 'pukka'
+// console.log(d)
 
+
+// 6i
+const a = Math.random()
+console.log(a)
+
+ const taxminQiling = 'pukka'
+
+ if (a === 'taxminQiling'){
+     console.log('Siz galaba qozondingiz')
+ } else {
+     console.log('Yoqotdingiz')
+ }
+
+const a = Math.random()
+console.log(a)
+const taxmin = a === 'pukka'?'Siz galaba qozondiz': 'Yoqotdingiz'
+console.log(taxmin)
