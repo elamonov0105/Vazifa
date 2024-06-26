fc1ff75587c3c6953a28e439fa28773f96500494
EZ7-easy <b.sh.tutor@gmail.com>
2024-06-26 10:50:07 +0500

    vazifa

---
+++ vazifa.js
@@ -0,0 +1,46 @@
+// 6a
+//   const soat = 10
+//
+//  if (soat < 14) {
+//     console.log('xayrli ton')
+//   } else if (soat < 19) {
+//     console.log('xayrili kun')
+//     } else {
+//     console.log('xayrili tun')
+//   }
+
+//6c
+//  const soat = 10
+//  const name = 'BUNYODbek'
+//
+// if (soat < 13) {
+//      console.log(Xayrli tong ${name} )
+//  } else if (soat < 19) {
+//      console.log(Xayrli kun ${name})
+//  } else {
+//      console.log(Xayrli tun ${name})
+//  }
+// const a = soat < 13 ? Xayrli tong ${name} : soat < 19 ? Xayrli kun ${name} : Xayrli tun ${name}
+// console.log(a)
+
+
+
+
+// 6D
+//     const yosh =
+//
+//  const a = yosh <= 6
+//   const b = yosh >= 65
+//
+//  if (a || b) {
+//     console.log('chegirma')
+//  }else{
+//     console.log('chegirma yoq')
+//   }
+//
+//     console.log(yosh <= 6 || yosh >= 65)
+//  const c = a || b ? 'chegirma' : 'chegirma yoq'
+// console.log(c)
+
+
+
