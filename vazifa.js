99c3729b84f8c16a959dae956e1ee36515474a93
EZ7-easy <b.sh.tutor@gmail.com>
2024-06-28 10:53:14 +0500

    vazifa

--- vazifa.js
+++ vazifa.js
@@ -1,148 +1,177 @@
-// 6a
-  const soat = 10
-
- if (soat < 14) {
-    console.log('xayrli ton')
-  } else if (soat < 19) {
-    console.log('xayrili kun')
-    } else {
-    console.log('xayrili tun')
-  }
-
-
- // 6b
-const hour = 10
-
-if (hour < 14) {
-    console.log('xayrli ton')
-} else if (hour < 19) {
-    console.log('xayrili kun')
-} else {
-    console.log('xayrili tun')
-}
-
-
-// 6c
-
- const soat = 10
- const name = 'BUNYODbek'
-
-if (soat < 13) {
-     console.log('Xayrli tong' ${name});
- } else if (soat < 19) {
-     console.log('Xayrli kun'  ${name});
- } else {
-     console.log('Xayrli tun' ${name});
- }
-const a = soat < 13 ? 'Xayrli tong' ${name} : soat < 19 ? 'Xayrli kun' ${name} : 'Xayrli tun' ${name}
-console.log(a)
-
-
-
+// // 6a
+//   const soat = 10
 //
-// 6D
-    const yosh = 35
+//  if (soat < 14) {
+//     console.log('xayrli ton')
+//   } else if (soat < 19) {
+//     console.log('xayrili kun')
+//     } else {
+//     console.log('xayrili tun')
+//   }
+//
+//
+//  // 6b
+// const hour = 10
+//
+// if (hour < 14) {
+//     console.log('xayrli ton')
+// } else if (hour < 19) {
+//     console.log('xayrili kun')
+// } else {
+//     console.log('xayrili tun')
+// }
+//
+//
+// // 6c
+//
+//  const soat = 10
+//  const name = 'BUNYODbek'
+//
+// if (soat < 13) {
+//      console.log('Xayrli tong' ${name});
+//  } else if (soat < 19) {
+//      console.log('Xayrli kun'  ${name});
+//  } else {
+//      console.log('Xayrli tun' ${name});
+//  }
+// const a = soat < 13 ? 'Xayrli tong' ${name} : soat < 19 ? 'Xayrli kun' ${name} : 'Xayrli tun' ${name}
+// console.log(a)
+//
+//
+//
+// //
+// // 6D
+//     const yosh = 35
+//
+//  const a = yosh <= 6
+//   const b = yosh >= 65
+//
+//  if (a || b) {
+//     console.log('chegirma')
+//  }else{
+//     console.log('chegirma yoq')
+//   }
+//
+//     console.log(yosh <= 6 || yosh >= 65)
+//  const c = a || b ? 'chegirma' : 'chegirma yoq'
+// console.log(c)
+//
+// const a = Math.random()
+// console.log(a)
+//
+//
+// // 6e
+//  const isHoliday = true;
+//  const yosh = 20;
+//
+//   const a = yosh <= 6;
+//   const b = yosh >= 65;
+//
+//  if (isHoliday){
+//      console.log('chegirma mavjud emas');
+//  }else{
+//      console.log('chegirma mavjud');
+//  }
+//
+//  console.log(yosh <= 6 || yosh >= 65)
+//
+// const c = isHoliday === a||b ? 'chegirma mavjud emas' : 'chegirma mavjud';
+// console.log(c);
+//
+//
+// // 6f
+// if (a < 0.5){
+//     console.log('chikka');
+// } else {
+//     console.log('pukka');
+// }
+// const d = a < 0.5 ? 'chikka' : 'pukka';
+// console.log(d)
+//
+// // 6g
+// const a = Math.random()
+// console.log(a);
+//
+// if (a < 0.5){
+//     console.log('chikka');
+// } else {
+//     console.log('pukka');
+// }
+//
+// // 6h
+// let result = "chikka" ? AfkUk : "pukka";
+//
+//
+//
+// // 6i
+// const a = Math.random();
+// console.log(a);
+//
+//  const taxminQiling = 'pukka';
+//
+//  if (a === 'taxminQiling'){
+//      console.log('Siz galaba qozondingiz');
+//  } else {
+//      console.log('Yoqotdingiz');
+//  }
+//
+// const a = Math.random();
+// console.log(a);
+// const taxmin = a === 'pukka'?'Siz galaba qozondiz': 'Yoqotdingiz';
+// console.log(taxmin);
+//
+//
+//
+//
+// // 7a
+//
+// function salomlashish (){
+//      const a = 'Salom';
+//     console.log(a);
+// }
+// salomlashish();
+//
+// // 7b
+//
+// const name = 'Simon';
+// function salomlashish (name){
+//     console.log(Salom ${name});
+// }
+// salomlashish(name);
+//
+// // 7c
+//
+// const salom = 'Assalomu alaykum aniqlanmagan!';
+// function salomlashish (name){
+//
+// }
+// salomlashish();
 
- const a = yosh <= 6
-  const b = yosh >= 65
+// 7d
+// function convertFahrenheit(celsius) {
+//     return (celsius * 9 / 5) + 32;
+// }
+//
+// const result = convertFahrenheit(25);
+// console.log(result);
 
- if (a || b) {
-    console.log('chegirma')
- }else{
-    console.log('chegirma yoq')
-  }
+// 7e
 
-    console.log(yosh <= 6 || yosh >= 65)
- const c = a || b ? 'chegirma' : 'chegirma yoq'
-console.log(c)
-
-const a = Math.random()
-console.log(a)
-
-
-// 6e
- const isHoliday = true;
- const yosh = 20;
-
-  const a = yosh <= 6;
-  const b = yosh >= 65;
-
- if (isHoliday){
-     console.log('chegirma mavjud emas');
- }else{
-     console.log('chegirma mavjud');
- }
-
- console.log(yosh <= 6 || yosh >= 65)
-
-const c = isHoliday === a||b ? 'chegirma mavjud emas' : 'chegirma mavjud';
-console.log(c);
-
-
-// 6f
-if (a < 0.5){
-    console.log('chikka');
-} else {
-    console.log('pukka');
-}
-const d = a < 0.5 ? 'chikka' : 'pukka';
-console.log(d)
-
-// 6g
-const a = Math.random()
-console.log(a);
-
-if (a < 0.5){
-    console.log('chikka');
-} else {
-    console.log('pukka');
-}
-
-// 6h
-let result = "chikka" ? AfkUk : "pukka";
-
-
-
-// 6i
-const a = Math.random();
-console.log(a);
-
- const taxminQiling = 'pukka';
-
- if (a === 'taxminQiling'){
-     console.log('Siz galaba qozondingiz');
- } else {
-     console.log('Yoqotdingiz');
- }
-
-const a = Math.random();
-console.log(a);
-const taxmin = a === 'pukka'?'Siz galaba qozondiz': 'Yoqotdingiz';
-console.log(taxmin);
+// function convertToCelsius(Farengeyt) {
+//     return (Farengeyt - 32) * 5 / 9;
+// }
+//
+// console.log(convertToCelsius(86));
 
 
 
 
-// 7a
 
-function salomlashish (){
-     const a = 'Salom';
-    console.log(a);
-}
-salomlashish();
 
-// 7b
 
-const name = 'Simon';
-function salomlashish (name){
-    console.log(Salom ${name});
-}
-salomlashish(name);
 
-// 7c
 
-const salom = 'Assalomu alaykum aniqlanmagan!';
-function salomlashish (name){
 
-}
-salomlashish();
+
+
+
